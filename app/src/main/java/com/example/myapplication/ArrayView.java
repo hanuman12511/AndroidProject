a385b52c3b1ca2e921846cc070f914f3c613e234
hanuman12511 <er.hanumansaini@gmail.com>
2023-07-25 07:21:27 +0530

    dsds

--- app/src/main/java/com/example/myapplication/ArrayView.java
+++ app/src/main/java/com/example/myapplication/ArrayView.java
@@ -11,17 +11,21 @@ public class ArrayView extends ArrayAdapter<String> {
 
     private final Activity context;
     private final String[] maintitle;
-    public ArrayView(Activity context,String []maintitle){
+    private final String[] subtitle;
+    public ArrayView(Activity context,String []maintitle,String []subtitle){
         super(context,R.layout.item_list,maintitle);
         this.context=context;
         this.maintitle=maintitle;
+        this.subtitle=subtitle;
     }
     public View getView(int pos, View view, ViewGroup viewgroup){
 
         LayoutInflater li=context.getLayoutInflater();
         View rowView=li.inflate(R.layout.item_list,null,true);
         TextView tv=rowView.findViewById(R.id.tv1);
+        TextView tv2=rowView.findViewById(R.id.tv2);
         tv.setText(maintitle[pos]);
+        tv2.setText(subtitle[pos]);
         return rowView;
     }
 }
--- app/src/main/java/com/example/myapplication/DrawerScreen.java
+++ app/src/main/java/com/example/myapplication/DrawerScreen.java
@@ -11,14 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DrawerScreen  extends AppCompatActivity {
 String data[] = {"Form","ImageView","Video","Audio"};
+    String data1[] = {"Form1","ImageView1","Video1","Audio1"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer);
 
         ListView lv = findViewById(R.id.listid);
-ArrayView ar = new ArrayView(this,data);
-     //   ArrayAdapter<String> str = new ArrayAdapter<>(getApplicationContext(),R.layout.item_list,R.id.tv1,data);
+ArrayView ar = new ArrayView(this,data,data1);
+      // ArrayAdapter<String> str = new ArrayAdapter<>(getApplicationContext(),R.layout.item_list,R.id.tv1,data);
 lv.setAdapter(ar);
 
     }
